9fc1b9559e63c19ae726e28958d54f81039cf3d9
NoahCardoza <noahcardoza@gmail.com>
2020-11-17 17:39:18 -0800

    load tachyons css

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -7,7 +7,13 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://unpkg.com/tachyons@4/css/tachyons.min.css',
+      },
+    ],
   },
 
   env: {
