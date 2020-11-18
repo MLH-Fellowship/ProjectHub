9aa5eabfa97f97d2004522b0388477bab97e42d6
NoahCardoza <noahcardoza@gmail.com>
2020-11-17 17:21:50 -0800

    throw box around button on login page

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
-    <LoginBox />
+    <el-card class="login-box-card">
+      <div slot="header" class="clearfix">
+        <span>Login</span>
+      </div>
+      <LoginBox />
+    </el-card>
   </div>
 </template>
 
@@ -24,25 +29,9 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.login-box-card {
+  width: 80%;
+  max-width: 400px;
+  min-width: 200px;
 }
 </style>
