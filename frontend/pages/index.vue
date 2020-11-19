3a55206660bab836b80eddd582aaf6a0025f7167
NoahCardoza <noahcardoza@gmail.com>
2020-11-19 04:21:01 -0800

    make landing page more responsive

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container overflow-hidden">
-    <el-row class="items-center mw8">
-      <el-col :span="16">
-        <div class="tl">
+  <div class="container overflow-hidden pa3" style="justify-content: none">
+    <el-row class="items-center w-100">
+      <el-col :offset="2" :xs="22" :sm="14" :md="10">
+        <div class="tl relative z-1">
           <p class="f1">Welcome to</p>
           <p class="b mv3" style="font-size: 5em">Project Hub</p>
           <p class="mt5">
@@ -14,10 +14,10 @@
           </div>
         </div>
       </el-col>
-      <el-col :span="8">
+      <el-col :xs="1" :sm="8" :md="12" class="hidden-xs-only">
         <el-image
-          class="relative"
-          style="top: 50px; left: -300px; width: 700px"
+          class="relative z-0"
+          style="top: 50px; left: -100px; width: 120%"
           :src="require('~/assets/images/humans1.png')"
         ></el-image>
       </el-col>
@@ -28,6 +28,7 @@
 <script>
 import { mapState } from 'vuex';
 import GithubLoginButton from '@/components/GithubLoginButton';
+import 'element-ui/lib/theme-chalk/display.css';
 
 export default {
   name: 'LandingPage',
