840007f8dd168f9194a25b9d305af1ab55694956
Noah Cardoza <noahcardoza@gmail.com>
2020-11-19 06:46:14 -0800

    🔨  [FIX] Remove Vuetify (#13)
    * remove plugin from config
    
    * remove vuetify refs in dashboard
    
    * remove most vuetify refs
    
    * install iconify
    
    * remove @nuxt/vuetify
    
    * replace svgs with iconify
    
    * replace help wanted svg with html
    
    * replace avatar svg with actual img tag
    
    * replace v-chips with el-tags
    
    * clean up cards
    
    * update bookmarking callback
    
    * make card mobile friendly
    
    * add selects and flex box to cards
    
    * rename Card to ProjectCard

---
+++ frontend/components/ProjectCard.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="project-card tl">
+    <div class="help-wanted-tag">HELP WANTED</div>
+    <el-card v-loading="loading" :body-style="{ padding: '0px' }">
+      <img
+        height="250"
+        src="https://socialify.git.ci/calvinqc/trivin/image?forks=1&issues=1&language=1&owner=1&pulls=1&stargazers=1&theme=Light"
+        alt="Project Banner"
+      />
+      <div class="ph3 pb3">
+        <div class="flex flex-row items-center justify-between f3 lh-copy">
+          <div>Event-bot</div>
+          <iconify-icon :icon="bookmarkIcon" @click="bookmark" />
+        </div>
+
+        <div class="mv3">
+          Discord Bot to make announcements about upcoming sessions for the
+          Fellows using Google Calendar.
+        </div>
+
+        <div class="flex flex-row items-center f3 lh-copy">
+          <iconify-icon inline icon="tag-multiple" height="24" class="mr2" />
+          Tags
+        </div>
+
+        <div class="mt3 mb2">
+          <el-tag color="#8991dc" class="project-tag mb1">Javascript</el-tag>
+          <el-tag color="#238aea" class="project-tag mb1">Discord</el-tag>
+          <el-tag color="#6fd3de" class="project-tag mb1">Google</el-tag>
+          <el-tag color="#238aea" class="project-tag mb1">Python</el-tag>
+          <el-tag color="#6fd3de" class="project-tag mb1">Calendly</el-tag>
+          <el-tag color="#8991dc" class="project-tag mb1">Pod 1.0.1</el-tag>
+        </div>
+        <div class="flex flex-row items-center">
+          <el-avatar
+            size="small"
+            src="https://avatars2.githubusercontent.com/u/38268331?s=460&u=121b7bd7d9dc5697e4728b21c34358cf416edf37&v=4"
+          />
+          <div class="ml2">
+            Created 5 days ago by
+            <el-button type="text" @click="$router.push('/NoahCardoza')">
+              @NoahCardoza
+            </el-button>
+          </div>
+        </div>
+
+        <div>
+          <el-button type="text" @click="learn_more">Learn More</el-button>
+        </div>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import IconifyIcon from '@iconify/vue';
+import BookmarkOutline from '@iconify/icons-mdi/bookmark-outline';
+import Bookmark from '@iconify/icons-mdi/bookmark';
+import TagMultiple from '@iconify/icons-mdi/tag-multiple';
+
+IconifyIcon.addIcon('bookmark-outline', BookmarkOutline);
+IconifyIcon.addIcon('bookmark', Bookmark);
+IconifyIcon.addIcon('tag-multiple', TagMultiple);
+
+export default {
+  name: 'ProjectCard',
+  components: {
+    IconifyIcon,
+  },
+  props: {
+    index: { type: Number, required: true },
+  },
+  data: () => ({
+    loading: false,
+    bookmarked: false,
+  }),
+  computed: {
+    bookmarkIcon() {
+      return this.bookmarked ? 'bookmark' : 'bookmark-outline';
+    },
+  },
+  methods: {
+    learn_more() {},
+    bookmark() {
+      this.bookmarked = !this.bookmarked;
+      // send request to backend
+    },
+  },
+};
+</script>
+
+<style>
+.project-card {
+  max-width: 400px;
+  min-width: 300px;
+}
+
+.help-wanted-tag {
+  padding: 0.5rem;
+  border-radius: 10px 10px 0 0;
+  background: #ffbe55;
+  width: max-content;
+  color: #fff;
+}
+
+.project-tag {
+  color: #fff;
+  border: none;
+  border-radius: 1rem;
+}
+</style>
--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -1,69 +1,106 @@
 <template>
-  <v-container class="grey lighten-5">
-    <h1>Filter By:</h1>
-    <!-- <DropdownMenu :name="pod" :options="pods" />
-    <DropdownMenu :name="language" :options="languages" /> -->
-    <br />
-    <br />
-    <hr />
-    <br />
-    <v-row no-gutters>
-      <v-col
-        v-for="(something, index) in 6"
-        :key="index"
-        cols="8"
-        sm="6"
-        md="4"
-      >
-        <Card :index="index" style="margin-bottom: 75px; padding-right: 25px" />
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="container pt4" style="align-items: none">
+    <div>
+      <div class="flex flex-row flex-wrap justify-center">
+        <div class="f3 lh-copy">Filters:</div>
+        <el-select
+          v-model="pods"
+          multiple
+          filterable
+          allow-create
+          default-first-option
+          placeholder="Pods"
+          class="mh3"
+        >
+          <el-option
+            v-for="item in options.pods"
+            :key="item"
+            :label="item"
+            :value="item"
+          ></el-option>
+        </el-select>
+        <el-select
+          v-model="languages"
+          multiple
+          filterable
+          allow-create
+          default-first-option
+          placeholder="Lanuages"
+        >
+          <el-option
+            v-for="item in options.languages"
+            :key="item"
+            :label="item"
+            :value="item"
+          ></el-option>
+        </el-select>
+      </div>
+      <div class="grid w-100 pa3">
+        <ProjectCard
+          v-for="(something, index) in 6"
+          :key="index"
+          :index="index"
+          class="ma3"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import ProjectCard from '@/components/ProjectCard';
+
 export default {
+  name: 'Dashboard',
+  componentd: { ProjectCard },
   data() {
     return {
-      pods: [
-        '1.0.0',
-        '1.0.1',
-        '1.0.2',
-        '1.0.3',
-        '1.0.5',
-        '1.0.6',
-        '1.1.0',
-        '1.1.1',
-        '1.1.2',
-        '1.1.3',
-        '1.1.5',
-        '1.1.6',
-        '1.2.0',
-        '1.2.1',
-        '1.2.2',
-      ].map((pod) => {
-        return { value: pod, label: pod };
-      }),
-      languages: [
-        'Javascript',
-        'Python',
-        'Java',
-        'Scala',
-        'Golang',
-        'Swift',
-        'C',
-        'C++',
-        'C#',
-        'OCaml',
-        'PHP',
-        'Rust',
-        'Julia',
-        'Cobol',
-      ].map((pod) => {
-        return { value: pod, label: pod };
-      }),
-      value: [],
+      options: {
+        pods: [
+          '1.0.0',
+          '1.0.1',
+          '1.0.2',
+          '1.0.3',
+          '1.0.5',
+          '1.0.6',
+          '1.1.0',
+          '1.1.1',
+          '1.1.2',
+          '1.1.3',
+          '1.1.5',
+          '1.1.6',
+          '1.2.0',
+          '1.2.1',
+          '1.2.2',
+        ],
+        languages: [
+          'Javascript',
+          'Python',
+          'Java',
+          'Scala',
+          'Golang',
+          'Swift',
+          'C',
+          'C++',
+          'C#',
+          'OCaml',
+          'PHP',
+          'Rust',
+          'Julia',
+          'Cobol',
+        ],
+      },
+      pods: [],
+      languages: [],
     };
   },
 };
 </script>
+
+<style scoped>
+.grid {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+}
+</style>
