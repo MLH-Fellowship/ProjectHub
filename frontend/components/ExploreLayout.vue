bd20e6011de8a70bc19e98e1d50825047d4a1ee0
Calvin Nguyen <calvin.nvqc@gmail.com>
2020-11-19 15:17:14 -0800

    complete profile page

---
+++ frontend/components/ExploreLayout.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container pt4" style="align-items: none">
+    <div>
+      <div class="flex flex-row flex-wrap justify-center">
+        <div class="f3 lh-copy">Filters:</div>
+        <el-select
+          v-model="pods"
+          multiple
+          filterable
+          allow-create
+          default-first-option
+          placeholder="Pods"
+          class="mh3"
+        >
+          <el-option
+            v-for="item in options.pods"
+            :key="item"
+            :label="item"
+            :value="item"
+          ></el-option>
+        </el-select>
+        <el-select
+          v-model="languages"
+          multiple
+          filterable
+          allow-create
+          default-first-option
+          placeholder="Lanuages"
+        >
+          <el-option
+            v-for="item in options.languages"
+            :key="item"
+            :label="item"
+            :value="item"
+          ></el-option>
+        </el-select>
+      </div>
+      <div class="grid w-100 pa3">
+        <ProjectCard
+          v-for="(something, index) in 6"
+          :key="index"
+          :index="index"
+          class="ma3"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProjectCard from '@/components/ProjectCard';
+
+export default {
+  name: 'Dashboard',
+  componentd: { ProjectCard },
+  data() {
+    return {
+      options: {
+        pods: [
+          '1.0.0',
+          '1.0.1',
+          '1.0.2',
+          '1.0.3',
+          '1.0.5',
+          '1.0.6',
+          '1.1.0',
+          '1.1.1',
+          '1.1.2',
+          '1.1.3',
+          '1.1.5',
+          '1.1.6',
+          '1.2.0',
+          '1.2.1',
+          '1.2.2',
+        ],
+        languages: [
+          'Javascript',
+          'Python',
+          'Java',
+          'Scala',
+          'Golang',
+          'Swift',
+          'C',
+          'C++',
+          'C#',
+          'OCaml',
+          'PHP',
+          'Rust',
+          'Julia',
+          'Cobol',
+        ],
+      },
+      pods: [],
+      languages: [],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.grid {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+}
+</style>
--- frontend/pages/_username/index.vue
+++ frontend/pages/_username/index.vue
@@ -1,20 +1,85 @@
 <template>
-  <div class="container">
-    <div>
-      <el-avatar :size="120" :src="user.avatar_url"></el-avatar>
+  <div>
+    <br />
+    <div style="margin-left: 200px">
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content">
+            <div class="relative" style="height: 270px">
+              <el-image
+                class="absolute"
+                style="top: 0; left: 0; width: 400px"
+                :src="require('~/assets/images/blob1.png')"
+              ></el-image>
+              <el-avatar
+                class="absolute ma4"
+                style="top: 0; left: 0"
+                icon="el-icon-user-solid"
+                :size="200"
+              ></el-avatar>
+            </div>
+          </div>
+        </el-col>
+        <el-col class="z-999 relative" :span="14">
+          <div class="grid-content">
+            <h2>Calvin Nguyen</h2>
+
+            <el-form class="mv4" :model="form" label-width="100px">
+              <el-form-item class="tl" label="Github:">
+                https://github.com/calvinqc
+              </el-form-item>
+              <el-form-item class="tl" label="Pod #:">1.0.1</el-form-item>
+
+              <el-form-item class="tl" label="My Interests">
+                <el-select
+                  v-model="form.interests"
+                  multiple
+                  placeholder="Select"
+                >
+                  <el-option
+                    v-for="interest in options.interests"
+                    :key="interest.value"
+                    :label="interest.label"
+                    :value="interest.value"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+
+              <el-form-item class="tl" label="My Skills">
+                <el-select v-model="form.skills" multiple placeholder="Select">
+                  <el-option
+                    v-for="skill in options.skills"
+                    :key="skill.value"
+                    :label="skill.label"
+                    :value="skill.value"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="About Me">
+                <el-input
+                  v-model="form.bio"
+                  type="textarea"
+                  placeholer="Write a short bio..."
+                ></el-input>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+      </el-row>
     </div>
-    <div>
-      <a :href="user_github_url">
-        <h1>{{ user.name }}</h1>
-      </a>
-    </div>
+
+    <hr />
+    <ExploreLayout />
   </div>
 </template>
 
 <script>
+// import { mapState } from 'vuex';
 import pick from 'ramda/src/pick';
+import ExploreLayout from '../../components/ExploreLayout.vue';
 
 export default {
+  components: { ExploreLayout },
   async asyncData({ params, $axios, $github }) {
     // API request to fill out page
     const details = {
@@ -22,7 +87,6 @@ export default {
       pods: ['1.0.1', '1.2.2'],
       projects: ['NoahCardoza/CaptchaHarvester'],
     };
-
     const [user, projects] = await Promise.all([
       $github
         .get('users', params.username)
@@ -61,10 +125,33 @@ export default {
         )
       ),
     ]);
-
     return {
       user,
       projects,
+    };
+  },
+  data() {
+    return {
+      step: 0,
+      visible: true,
+      options: {
+        interests: [
+          { label: 'Front-end', value: 1 },
+          { label: 'Back-end', value: 2 },
+          { label: 'Python', value: 3 },
+        ],
+        skills: [
+          { label: 'Android', value: 1 },
+          { label: 'ML/AI', value: 2 },
+          { label: 'Healthcare', value: 3 },
+        ],
+      },
+      form: {
+        skills: ['Android', 'Java', 'Javascript'],
+        interests: ['Back-end', 'Networking'],
+        bio:
+          "I'm a senior software engineering student at SJSU, and a current MLH at Pod 1.0.1",
+      },
     };
   },
   computed: {
@@ -72,7 +159,28 @@ export default {
       return `https://github.com/${this.user.login}`;
     },
   },
+  // computed: mapState(['user']),
+  methods: {
+    next() {
+      this.step++;
+    },
+    back() {
+      this.step--;
+    },
+
+    submit() {
+      console.log(this.form);
+      this.visible = false;
+      this.$emit('complete');
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.grid {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+}
+</style>
