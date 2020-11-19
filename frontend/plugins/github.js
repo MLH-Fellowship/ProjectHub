72423dae55eebc2b3990868599548ef7856af62a
NoahCardoza <noahcardoza@gmail.com>
2020-11-19 13:01:10 -0800

    inject $github plugin

--- frontend/plugins/github.js
+++ frontend/plugins/github.js
@@ -1,17 +1,11 @@
-import Vue from 'vue';
-
-export default ({ $axios }) => {
-  Vue.use({
-    install(Vue) {
-      Vue.prototype.$github = {
-        get(...params) {
-          return $axios.$get(`https://api.github.com/${params.join('/')}`, {
-            headers: {
-              Accept: 'application/vnd.github.mercy-preview+json',
-            },
-          });
+export default ({ $axios }, inject) => {
+  inject('github', {
+    get(...params) {
+      return $axios.$get(`https://api.github.com/${params.join('/')}`, {
+        headers: {
+          Accept: 'application/vnd.github.mercy-preview+json',
         },
-      };
+      });
     },
   });
 };
