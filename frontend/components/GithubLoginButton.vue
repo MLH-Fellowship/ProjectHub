6c4a58a882dfd1e350b69dd9a2064c0605a732a1
NoahCardoza <noahcardoza@gmail.com>
2020-11-18 18:41:01 -0800

    respect redirects

--- frontend/components/GithubLoginButton.vue
+++ frontend/components/GithubLoginButton.vue
@@ -11,6 +11,11 @@ export default {
     async authenticate() {
       const { code } = await this.$auth.authenticate('github');
       await this.$store.dispatch('user/login', code);
+
+      if (this.$route.hash && this.$route.hash.startsWith('#redirect=')) {
+        return this.$router.push(this.$route.hash.slice(10));
+      }
+
       this.$router.push('/');
     },
   },
