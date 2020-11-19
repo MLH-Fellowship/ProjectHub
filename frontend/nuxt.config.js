bd99045e8406ec630a323452e9ac381df3c93449
NoahCardoza <noahcardoza@gmail.com>
2020-11-19 05:06:40 -0800

    remove plugin from config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -43,7 +43,6 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
