e9028be2473b97b70e902fba8f2b148d7c904d96
NoahCardoza <noahcardoza@gmail.com>
2020-11-19 03:20:18 -0800

    add isLoggedIn to user store to aid in on-boarding

--- frontend/store/user.js
+++ frontend/store/user.js
@@ -6,12 +6,16 @@ const metaFactory = () => ({
 
 export const state = () => ({
   meta: metaFactory(),
+  isLoggedIn: false,
   token: null,
 });
 
 export const mutations = {
   SET_TOKEN(state, token) {
     state.token = token;
+  },
+  SET_IS_LOGGED_IN(state, isLoggedIn) {
+    state.isLoggedIn = isLoggedIn;
   },
   SET_META(state, meta) {
     state.meta = meta;
@@ -20,13 +24,20 @@ export const mutations = {
 
 export const actions = {
   async login({ commit }, code) {
+    // onBoarding
     const { token, meta } = await this.$axios.$get(`/api/auth/${code}`);
 
     commit('SET_TOKEN', token);
     commit('SET_META', meta);
+
+    return {
+      onBoarding: true,
+      finish: () => commit('SET_IS_LOGGED_IN', true),
+    };
   },
   logout({ commit }) {
     commit('SET_TOKEN', null);
     commit('SET_META', metaFactory());
+    commit('SET_IS_LOGGED_IN', false);
   },
 };
