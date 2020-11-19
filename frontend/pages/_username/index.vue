95094263b7e4205f17614a8fe9234e932360787e
NoahCardoza <noahcardoza@gmail.com>
2020-11-18 20:26:28 -0800

    removed container class

--- frontend/pages/_username/index.vue
+++ frontend/pages/_username/index.vue
@@ -75,13 +75,4 @@ export default {
 };
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-</style>
+<style></style>
