60c3102764f7af1b45a988ddc849251e0295f9f8
Noah Cardoza <noahcardoza@gmail.com>
2020-11-20 07:01:56 -0800

    🦾 [FEAT] Project detail card (#17)
    * Added the project detail card
    
    * index image v-for by url
    
    * fix uncloseable model
    
    * create global hexHash filter
    
    * extract ProjectOverview to new component
    
    * make model dynamic
    
    * factor BookmarkButton to new comp
    
    * stop event propagation on bookmark
    
    Co-authored-by: gracewgao <ggaoww@gmail.com>

---
+++ frontend/components/ProjectOverview/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="pa3 tl">
+    <p class="f4 b mb3">Project Overview</p>
+
+    <div class="flex flex-row f5 mt3">
+      <iconify-icon icon="code" class="mr2" />
+      {{ details.languages.join(', ') }}
+    </div>
+
+    <div class="flex flex-row f5 mt3">
+      <iconify-icon icon="group" class="mr2" />
+      Jordan, Noah, Calvin
+    </div>
+
+    <div class="flex flex-row f5 mt3">
+      <iconify-icon icon="github" class="mr2" />
+      <a href="details.source">View Github repo</a>
+    </div>
+
+    <div class="flex flex-row f5 mt3">
+      <iconify-icon icon="tags" class="mr2" />
+      Tags
+    </div>
+
+    <div v-for="tag in details.tags" :key="tag" class="mt3 mb2"></div>
+
+    <div class="mt3 mb2">
+      <el-tag
+        v-for="tag in details.tags"
+        :key="tag"
+        :color="tag | hexHash"
+        class="project-tag mb1"
+      >
+        {{ tag }}
+      </el-tag>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconifyIcon from '@iconify/vue';
+import TagMultiple from '@iconify/icons-mdi/tag-multiple';
+import Clock from '@iconify/icons-mdi/clock-time-four-outline';
+import Group from '@iconify/icons-mdi/account-group';
+import Github from '@iconify/icons-mdi/github';
+
+IconifyIcon.addIcon('tags', TagMultiple);
+IconifyIcon.addIcon('clock', Clock);
+IconifyIcon.addIcon('group', Group);
+IconifyIcon.addIcon('github', Github);
+
+export default {
+  name: 'ProjectOverview',
+  components: {
+    IconifyIcon,
+  },
+  props: {},
+  data() {
+    return {
+      details: {
+        title: 'Project Title',
+        source: 'https://github.com/MLH-Fellowship/ProjectHub',
+        tags: ['Python', 'Pod 1.0.1', 'Discord'],
+        languages: ['Python', 'Javascript'],
+      },
+    };
+  },
+  computed: {},
+  mounted() {},
+  beforeDestroy() {},
+  methods: {},
+};
+</script>
+
+<style lang="css" scoped>
+.el-tag {
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
+
+.input-new-tag {
+  width: 90px;
+}
+
+.project-tag {
+  color: #fff;
+  border: none;
+  border-radius: 1rem;
+}
+</style>
