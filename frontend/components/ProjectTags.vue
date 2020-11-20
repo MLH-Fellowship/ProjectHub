810ac7b0a15007c9e9afd777c985c5ca53b4d233
NoahCardoza <noahcardoza@gmail.com>
2020-11-20 13:51:02 -0800

    create project tags component

---
+++ frontend/components/ProjectTags.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-tag
+      v-for="tag in tags"
+      :key="tag"
+      :color="tag | hexHash"
+      class="project-tag mb1"
+    >
+      {{ tag }}
+    </el-tag>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectTags',
+  props: {
+    tags: { type: Array, required: true },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.el-tag {
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
+
+.project-tag {
+  color: #fff;
+  border: none;
+  border-radius: 1rem;
+}
+</style>
