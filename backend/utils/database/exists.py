11dd5cf19e9c254bc8209bdd6c55ab9728d521ea
NoahCardoza <noahcardoza@gmail.com>
2020-11-19 18:22:56 -0800

    fix db exists methods for the 100th time

--- backend/utils/database/exists.py
+++ backend/utils/database/exists.py
@@ -4,12 +4,12 @@ from . import connection
 def project(rid):
     conn = connection.create()
     cur = conn.cursor()
-    cur.execute("SELECT id FROM projects WHERE id=%d", (rid, ))
+    cur.execute("SELECT id FROM projects WHERE id=%s", (rid, ))
     return cur.fetchone() is not None
 
 
 def user(rid):
     conn = connection.create()
     cur = conn.cursor()
-    cur.execute("SELECT id FROM users WHERE id=%d", (rid,))
+    cur.execute("SELECT id FROM users WHERE id=%s", (rid,))
     return cur.fetchone() is not None
