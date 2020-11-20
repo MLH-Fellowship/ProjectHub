4042992616b4103f1ad812a1b8ecd5dadc2aaa50
NoahCardoza <noahcardoza@gmail.com>
2020-11-19 17:28:36 -0800

    update sql table defs

--- init.sql
+++ init.sql
@@ -1,2 +1,19 @@
-CREATE TABLE IF NOT EXISTS projects(id serial primary key, name text, description text, source_link text, demo_link text, tags text, authors text);
-CREATE TABLE IF NOT EXISTS users(username text primary key, fullname text, pods text, timezone text, bio text, skills text, interests text);
+CREATE TABLE IF NOT EXISTS projects(
+  id serial primary key,
+  name text,
+  description text,
+  source text,
+  demo text,
+  tags text
+);
+
+CREATE TABLE IF NOT EXISTS users(
+  id integer primary key, -- equal to github's ids
+  login text,
+  name text,
+  pods text,
+  timezone_offset smallint,
+  bio text,
+  skills text,
+  interests text
+);
