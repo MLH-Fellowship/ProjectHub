1b99519dd45d63027e68e3a8c75b03f21ad5029c
NoahCardoza <noahcardoza@gmail.com>
2020-11-21 15:50:06 -0800

    restrict gh scopes to base needs

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,20 +1,23 @@
 export default function ({ $axios, redirect, store, route }) {
   $axios.onRequest((config) => {
-    const { token } = store.state.user
+    const { token } = store.state.user;
     if (!token) {
-      return
+      return;
     }
 
-    if (config.url.startsWith('/') || config.url.startsWith(process.env.FRONTEND_BASE_URL)) {
-      config.headers.common.Authorization = `Bearer ${token}`
+    if (
+      config.url.startsWith('/') ||
+      config.url.startsWith(process.env.FRONTEND_BASE_URL)
+    ) {
+      config.headers.common.Authorization = `Bearer ${token}`;
     }
-  })
+  });
 
   $axios.onError((error) => {
-    const code = parseInt(error.response && error.response.status)
+    const code = parseInt(error.response && error.response.status);
     if (code === 403) {
       // TODO: show message when auth=expired on login page
-      redirect(`/?login=1&auth=expired&redirect=${route.path}`)
+      redirect(`/?login=1&auth=expired&redirect=${route.path}`);
     }
-  })
+  });
 }
--- frontend/plugins/vue-social-auth.js
+++ frontend/plugins/vue-social-auth.js
@@ -1,33 +1,17 @@
-import Vue from 'vue'
-import VueSocialauth from 'vue-social-auth'
+import Vue from 'vue';
+import VueSocialauth from 'vue-social-auth';
 
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios);
 
 Vue.use(VueSocialauth, {
   providers: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID,
       redirectUri: `${process.env.FRONTEND_BASE_URL}/callback/github`,
-      scope: [
-        'repo',
-        'admin:repo_hook',
-        'admin:org',
-        'admin:public_key',
-        'admin:org_hook',
-        'gist',
-        'notifications',
-        'user',
-        'delete_repo',
-        'write:discussion',
-        'write:packages',
-        'read:packages',
-        'delete:packages',
-        'admin:gpg_key',
-        'workflow',
-      ],
+      scope: ['read:user', 'read:org'],
     },
   },
-})
+});
